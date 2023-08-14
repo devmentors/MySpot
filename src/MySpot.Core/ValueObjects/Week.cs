a49825d69b4bec677927d15f60285a8cfca3ad03
MafistoPL <mateusz.poreba@outlook.com>
2023-08-14 22:33:05 +0200

    fix(Week.cs): correct the starting day of the week calculation
    The previous implementation of the Week class had an off-by-one error in calculating the starting day of the week. This commit corrects the calculation by adding 1 to the pastDays value. Additionally, a comprehensive set of unit tests has been added to ensure the proper behavior of the Week class.

--- src/MySpot.Core/ValueObjects/Week.cs
+++ src/MySpot.Core/ValueObjects/Week.cs
@@ -9,7 +9,7 @@ public sealed record Week
     {
         var pastDays = value.DayOfWeek is DayOfWeek.Sunday ? 7 : (int) value.DayOfWeek;
         var remainingDays = 7 - pastDays;
-        From = new Date(value.AddDays(-1 * pastDays));
+        From = new Date(value.AddDays(-1 * pastDays + 1));
         To = new Date(value.AddDays(remainingDays));
     }
 
